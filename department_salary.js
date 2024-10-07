0509bdde93cd6023d0e9b6a2b6b4fc4d8d97f33c
Cameron Smith <Cs11@usf.edu>
2024-10-06 22:03:12 -0400

    Initialize company structure with departments and employees

---
+++ department_salary.js
@@ -0,0 +1,57 @@
+// Task 1: Create a Department Structure
+
+const company = {
+    departments: [
+        {
+            departmentName: 'Engineering',
+            employees: [
+                {
+                    name: 'Parker',
+                    salary: 80000,
+                    subordinates: [
+                        {
+                            name: 'Collin'
+                            salary: 95000,
+                            subordinates: []
+                         }
+                    ]
+                }
+            ]
+        }
+},
+                {
+                    name: 'Alex',
+                    salary: 65000,
+                    subordinates: []
+
+                }
+            ]
+        },
+        {
+            departmentName: 'Sales',
+            employees: [
+                {
+                    name: 'Rachael',
+                    salary: 150000,
+                    subordinates: [
+                        {
+                            name: 'Frank',
+                            salary: 40000,
+                            subordinates: []
+                        }
+                    ]
+                }
+                {   
+                    name: 'Samantha',
+                    salary: 70000,
+                    subordinates: []
+                 }
+            ]
+        }
+    ]
+;
+        
+
+        
+            
+        
